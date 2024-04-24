9d2e827179ae74de5922440d7ba6e22df24a8ed0
Aroob1am <aroob.em@gmail.com>
2024-04-24 16:59:40 +0300

    logout

--- app/src/main/java/com/example/sweprojects2/HomePageF.java
+++ app/src/main/java/com/example/sweprojects2/HomePageF.java
@@ -4,6 +4,7 @@ package com.example.sweprojects2;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +22,7 @@ public class HomePageF extends AppCompatActivity {
         LinearLayout service2Layout = findViewById(R.id.Service2);
         LinearLayout service3Layout = findViewById(R.id.Service3);
 // Assuming you have a reference to the Service1 view
-
+        Button logoutButton = findViewById(R.id.button4);
 // Set click listener
 
         // Set click listeners for the service LinearLayouts
@@ -48,8 +49,21 @@ public class HomePageF extends AppCompatActivity {
                 navigateToBookPage(clientId);
             }
         });
-    }
 
+
+
+
+     logoutButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Intent to start MainActivity
+            Intent mainActivityIntent = new Intent(HomePageF.this, MainActivity.class);
+            // Clear the back stack so the user can't navigate back to the HomePageF after logging out
+            mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(mainActivityIntent);
+        }
+    });
+}
     // Method to navigate to the book page activity
     private void navigateToBookPage(int clientId) {
         Intent intent = new Intent(HomePageF.this, book.class);
